7a405071bcaad79a6351462c072fc160f4f7b5c5
CristianF2005 <1482670@senati.pe>
2023-04-21 11:23:22 -0500

    contador

---
+++ practica1.js
@@ -0,0 +1,13 @@
+var countDDate = new Date(),
+            endday = new Date(), days =24;
+            countDDate.setDate(countDDate.getDate() - (countDDate.getDay() + 2)%7);
+            endday.setDate(countDDate.getDate() + days);
+            endday.getTime();
+            countDiv = document.getElementById('countdown');
+        var x = setInterval(function(){
+            var now = new Date().getTime();
+            var daterest = endday - now;
+            var days = Math.floor(daterest / (1000 * 60 * 60 * 24));
+            
+            countDiv.innerHTML = "faltan "+ days + " d ";
+        }, 1000);
